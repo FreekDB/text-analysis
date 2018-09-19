1722b86d8c954772d13c4a07295506971a99026a
Freek de Bruijn <freekdb@gmail.com>
2018-09-19 13:54:44 +0200

    Added first version of main class.

---
+++ src/main/java/TextAnalysis.java
@@ -0,0 +1,9 @@
+public class TextAnalysis {
+    public static void main(final String[] arguments) {
+        new TextAnalysis().run();
+    }
+
+    private void run() {
+        System.out.println("TextAnalysis.run is running!");
+    }
+}
